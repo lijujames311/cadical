298feca7c8fca36c8001cf5cd0b24da6ac994758
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2026-01-03 14:42:01 +0100

    remove useless member

--- src/internal.hpp
+++ src/internal.hpp
@@ -269,8 +269,6 @@ struct Internal {
   vector<int> minimized;     // removable or poison in 'minimize'
   vector<int> shrinkable;    // removable or poison in 'shrink'
   Reap reap;                 // radix heap for shrink
-  bool factorcheckdone =
-      0; // boolean indicated that we have done the resize 1
 
   vector<int> sweep_schedule; // remember sweep varibles to reschedule
   bool sweep_incomplete;      // sweep
