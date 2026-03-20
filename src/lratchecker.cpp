6fc7f98f1e449a7900ead534ac4cd4975c29002a
Mathias Fleury <fleury@cs.uni-freiburg.de>
2026-03-20 13:46:14 +0100

    add information

--- src/lratchecker.cpp
+++ src/lratchecker.cpp
@@ -271,6 +271,7 @@ bool LratChecker::check_resolution (vector<int64_t> proof_chain) {
       return false;
     }
     if (!checked_lit (lit)) {
+      LOG ("literal %d of the clause is not present in the resolvents.", lit);
       // learned clause is subsumed by resolvents
       // should only be triggered by following options.
       assert (internal->opts.vivifyinst || internal->opts.cover ||
